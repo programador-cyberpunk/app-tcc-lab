5c7e50e9d76a66300040b30e0d17bb296ecaadea
Marcos Barreiros <guimaraes.marcos@aluno.ifsp.edu.br>
2024-06-15 08:34:05 -0300

    operações Insert e Read do CRUD

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,15 +3,23 @@ import { RefresherCustomEvent } from '@ionic/angular';
 import { MessageComponent } from '../message/message.component';
 
 import { DataService, Message } from '../services/data.service';
-
+import axios from 'axios';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  postagem: string ='';
+  postagens: any = [];
   private data = inject(DataService);
-  constructor() {}
+  constructor() {
+    this.pegaPostagem();
+  }
+
+  postagemDados = {
+    postagem: ""
+  }
 
   refresh(ev: any) {
     setTimeout(() => {
@@ -21,5 +29,40 @@ export class HomePage {
 
   getMessages(): Message[] {
     return this.data.getMessages();
+  }
+
+  ngOnInit() {
+   
+  }
+
+  create() {
+    
+    console.log(this.postagemDados);
+    
+    axios.post("https://localhost/postagemConn.php", this.postagemDados)
+    .then(
+      (response) => [
+console.log(response),
+
+      ])
+    .catch((error: any ) => {
+      console.log(error);
+      
+    })
+    
+  }
+
+  pegaPostagem() {
+    axios.get("https://localhost/postagemConn.php")
+    .then(
+      (response) => [
+console.log(response.data),
+     this.postagens = response.data
+
+      ])
+    .catch((error: any ) => {
+      console.log(error);
+      
+    })
   }
 }
